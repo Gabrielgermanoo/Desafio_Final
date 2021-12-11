8d87fca5e8e616efee08b07d4ea8532df02ae818
Gabrielgermanoo <gabriel.lucasgermano@gmail.com>
2021-12-11 11:17:20 -0300

    banco criado com as tabelas

--- back_end/Sume/admin.py
+++ back_end/Sume/admin.py
@@ -4,11 +4,11 @@ from . import models
 from . import actions
 
 class FornecedoresForm(admin.ModelAdmin):
-    readonly_fields = ('id_user_cad', 'id_user_alt')
-    search_fields = ['razao_social', 'cnpj', 'email', 'telefone']
+    readonly_fields = ['id_user_cad', 'id_user_alt']
+    search_fields = ['razao_social','cnpj','email','telefone']
     list_filter = ['razao_social']
     actions = [actions.atualiza_fornecedores]
-
+    
     def save_model(self, request, obj, form, change):
         if change:
             obj.id_user_alt = request.user
@@ -17,15 +17,30 @@ class FornecedoresForm(admin.ModelAdmin):
 
         obj.save()
 
-admin.site.register(models.NaturezasDespesa)
-admin.site.register(models.Projetos)
+
+class ProjetosForm(admin.ModelAdmin):
+    search_fields = ['titulo']
+    list_display = ['titulo']
+    readonly_fields = ['id_user_cad','id_user_alt']
+
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.id_user_alt = request.user
+        
+        else:
+            obj.id_user_cad = request.user
+
+        obj.save()
+
 admin.site.register(models.Fornecedores, FornecedoresForm)
 admin.site.register(models.Ordens)
-admin.site.register(models.ItensOrdens)
 admin.site.register(models.Contratos)
-admin.site.register(models.ItensContrato)
+admin.site.register(models.ItensContratos)
+admin.site.register(models.Projetos, ProjetosForm)
+admin.site.register(models.NaturezasDespesa)
 admin.site.register(models.Vigencias)
-
+admin.site.register(models.ItensOrdem)
 
 
 
--- back_end/Sume/models.py
+++ back_end/Sume/models.py
@@ -2,108 +2,141 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.base import Model
 
-class NaturezasDespesa(models.Model):
-    id_natureza_despesa = models.AutoField(primary_key = True)
-    cod_natureza_despesa = models.CharField(max_length=8)
-    desc_natureza_despesa = models.CharField(max_length=60)
+class DadosCadModel(models.Model):
+    id_user_cad = models.ForeignKey(
+        User, on_delete=models.CASCADE,null=True,blank=True, related_name='%(class)s_user_cad')
+    dt_cad = models.DateField(auto_now_add=True)
+    id_user_alt = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True,blank=True, related_name='%(class)s_user_alt')
+    dt_alt = models.DateField(null=True, blank=True, auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Fornecedores(DadosCadModel):
+    id_fornecedor = models.AutoField(primary_key=True)
+    razao_social = models.CharField(null=False, max_length=60)
+    cnpj = models.CharField(max_length=30, unique=True)
+    email = models.CharField(max_length=60, unique=True)
+    telefone = models.CharField(max_length=12, null=True, blank=True)
+    
+    class Meta:
+        ordering = ['razao_social']
+        db_table = "Fornecedores"
+        verbose_name_plural = 'Fornecedores'
 
     def __str__(self) -> str:
-        return self.desc_natureza_despesa
+        return self.razao_social 
 
-class Projetos(models.Model):
-    id_projeto = models.AutoField(primary_key = True)
-    titulo = models.CharField(max_length = 100)
+class Projetos(DadosCadModel):
+    id_projeto = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=60)
     dt_inicio_vigencia = models.DateField()
     dt_fim_vigencia = models.DateField()
-    id_user_cad = models.ForeignKey(User, on_delete= models.CASCADE, related_name= 'user_cad')
-    dt_cad = models.DateField(auto_now_add = True)
-    id_user_alt = models.ForeignKey(User, on_delete= models.CASCADE, related_name= 'user_alt', null = True)
-    dt_alt = models.DateField(null = True, blank= True, auto_now = True)
+
+    class Meta:
+        ordering = ['titulo']
+        db_table = "Projetos"
+        verbose_name_plural = 'Projetos'
 
     def __str__(self) -> str:
         return self.titulo
 
-class Fornecedores(models.Model):
-    id_fornecedor = models.AutoField(primary_key= True)
-    razao_social = models.CharField(max_length= 100)
-    cnpj = models.CharField(max_length= 20, unique= True)
-    email = models.EmailField(max_length= 50)
-    telefone = models.CharField(max_length= 20)
-    id_user_cad = models.ForeignKey(User, on_delete= models.CASCADE, related_name= '%(class)s_user_cad')
-    dt_cad = models.DateField(auto_now_add = True)
-    id_user_alt = models.ForeignKey(User, on_delete= models.CASCADE, related_name= '%(class)s_user_alt', null = True)
-    dt_alt = models.DateField(null = True, blank= True, auto_now = True)
+
+class NaturezasDespesa(models.Model):
+    id_natureza_despesa = models.AutoField(primary_key=True)
+    cod_natureza_despesas = models.CharField(max_length=8)
+    desc_natureza_despesa = models.CharField(max_length=60)
+
+    class Meta:
+        ordering = ["desc_natureza_despesa"]
+        db_table = "NaturezasDespesa"
+        verbose_name_plural = "Naturezas Despesas"
 
     def __str__(self) -> str:
-        return self.razao_social
+        return self.desc_natureza_despesa
 
-class Ordens (models.Model):
-    id_ordem = models.AutoField(primary_key= True)
-    id_projeto = models.ForeignKey(Projetos, on_delete=models.Case) 
-    id_fornecedor = models.ForeignKey(Fornecedores, on_delete=models.Case)
-    id_natureza_despesa = models.ForeignKey(NaturezasDespesa, on_delete=models.Case)
+    
+class Ordens(DadosCadModel):
+    id_ordem = models.AutoField(primary_key=True)
+    id_projeto = models.ForeignKey(Projetos, on_delete=models.CASCADE, related_name='Ordens')
+    id_fornecedor = models.ForeignKey(Fornecedores, on_delete=models.CASCADE, related_name='Ordens')
+    id_natureza_despesa = models.ForeignKey(NaturezasDespesa, on_delete=models.CASCADE, related_name='Ordens')
     numero = models.IntegerField()
     ano = models.IntegerField()
-    id_user_cad = models.ForeignKey(User, on_delete= models.CASCADE, related_name= '%(class)s_user_cad')
-    dt_cad = models.DateField(auto_now_add = True)
-    id_user_alt = models.ForeignKey(User, on_delete= models.CASCADE, related_name= '%(class)s_user_alt', null = True)
-    dt_alt = models.DateField(null = True, blank= True, auto_now = True)
+    
+    class Meta:
+        ordering = ["ano"]
+        db_table = "Ordens"
+        verbose_name_plural = "Ordens"
 
+    #titulo_projeto - razao_social - numero
     def __str__(self) -> str:
-        return str(self.id_projeto) +'/' +str(self.id_fornecedor)
+        return self.id_projeto.titulo + ' - ' + self.id_fornecedor.razao_social + ' - ' + str(self.numero)    
 
-class ItensOrdens (models.Model):
-    id_item_ordem = models.AutoField(primary_key= True)
-    id_ordem = models.ForeignKey(Ordens, on_delete= models.Case)
-    produto_servico = models.CharField(max_length= 100)
-    qtd = models.IntegerField()
-    valor_unitario = models.IntegerField()
-    id_user_cad = models.ForeignKey(User, on_delete= models.CASCADE, related_name= '%(class)s_user_cad')
-    dt_cad = models.DateField(auto_now_add = True)
-    id_user_alt = models.ForeignKey(User, on_delete= models.CASCADE, related_name= '%(class)s_user_alt', null = True)
-    dt_alt = models.DateField(null = True, blank= True, auto_now = True)
 
-    def __str__(self) -> str:
-        return self.produto_servico
-
-class Contratos (models.Model):
-    id_contrato = models.AutoField(primary_key= True)
-    id_prestador = models.IntegerField()
+class Contratos(DadosCadModel):
+    id_contrato = models.AutoField(primary_key=True)
+    id_prestador = models.ForeignKey(Fornecedores, on_delete=models.CASCADE, related_name='Contratos')
     numero = models.IntegerField()
     ano = models.IntegerField()
-    data_inicio_vigencia = models.DateField()
-    data_fim_vigencia = models.DateField()
-    objeto = models.CharField(max_length= 25)
-    id_user_cad = models.ForeignKey(User, on_delete= models.CASCADE, related_name= '%(class)s_user_cad')
-    dt_cad = models.DateField(auto_now_add = True)
-    id_user_alt = models.ForeignKey(User, on_delete= models.CASCADE, related_name= '%(class)s_user_alt', null = True)
-    dt_alt = models.DateField(null = True, blank= True, auto_now = True)
+    objeto = models.CharField(max_length=30)
 
+    class Meta:
+        ordering = ["ano"]
+        db_table = "Contratos"
+        verbose_name_plural = "Contratos"
+
+    #IntelBras - 2021 - objeto
     def __str__(self) -> str:
-        return self.objeto
-
-class ItensContrato (models.Model):
-    id_item_contrato = models.AutoField(primary_key= True)
-    id_contrato = models.ForeignKey(Contratos, on_delete= models.Case)
-    id_item_ordem = models.ForeignKey(ItensOrdens, on_delete= models.Case)
-    qtd = models.IntegerField()
-    valor_unitario = models.IntegerField()
-    id_user_cad = models.ForeignKey(User, on_delete= models.CASCADE, related_name= '%(class)s_user_cad')
-    dt_cad = models.DateField(auto_now_add = True)
-    id_user_alt = models.ForeignKey(User, on_delete= models.CASCADE, related_name= '%(class)s_user_alt', null = True)
-    dt_alt = models.DateField(null = True, blank= True, auto_now = True)
+        return self.id_prestador.razao_social +' - '+ str(self.ano) + ' - '+ self.objeto           
 
 
-    def __str__(self) -> str:
-        return str(self.id_contrato) +'/' +str(self.id_item_ordem)
-
-class Vigencias (models.Model):
-    id_vigencia = models.AutoField(primary_key= True)
-    id_contrato = models.ForeignKey(Contratos, on_delete= models.Case)
+class Vigencias(models.Model):
+    id_vigencia = models.AutoField(primary_key=True)
+    id_contrato = models.ForeignKey(Contratos, on_delete=models.CASCADE)
     dt_inicio_vigencia = models.DateField()
     dt_fim_vigencia = models.DateField()
-    valor = models.IntegerField()
+
+    class Meta:
+        ordering = ['id_contrato']
+        db_table = "Vigencias"
+        verbose_name_plural = 'Vigencias'
+
+        def __str__(self) -> str:
+            return self.id_contrato
+
+
+class ItensOrdem(DadosCadModel):
+    id_item_ordem = models.AutoField(primary_key=True)
+    id_ordem = models.ForeignKey(Ordens, on_delete=models.CASCADE)
+    produto_servico = models.CharField(max_length=150)
+    qtd = models.IntegerField()
+    valor_unitario = models.FloatField()
+    vinculado = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ['produto_servico']
+        db_table = "ItensOrdem"
+        verbose_name_plural = 'Itens Ordem'
 
     def __str__(self) -> str:
-        return self.id_vigencia
+        return str(self.id_ordem) +' - '+ self.produto_servico
+
+
+class ItensContratos(DadosCadModel):
+    id_item_contrato = models.AutoField(primary_key=True)
+    id_contrato = models.ForeignKey(Contratos, on_delete=models.CASCADE)
+    id_item_ordem = models.ForeignKey(ItensOrdem, on_delete=models.CASCADE)
+    qtd = models.IntegerField()
+    valor_unitario = models.FloatField()
+    
+    class Meta:
+        ordering = ['id_contrato']
+        db_table = "ItensContratos"
+        verbose_name_plural = 'Itens Contratos'
+
+    def __str__(self) -> str:
+        return str(self.id_contrato)
         
